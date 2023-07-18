89271766878126be1ef7dcefb0ce92770f41f593
Jamison Coombs <jamiedude224@gmail.com>
2023-07-18 15:42:35 -0700

    Added dependencies and created firebase project

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'kc-thrift',
+    appId: '1:492869050671:web:ad7609dd713344ea1cc1da',
+    databaseURL: 'https://kc-thrift-default-rtdb.firebaseio.com',
+    storageBucket: 'kc-thrift.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDUZhy6M1NpH6jHCEc8AVytR_XEQUyXUWs',
+    authDomain: 'kc-thrift.firebaseapp.com',
+    messagingSenderId: '492869050671',
+    measurementId: 'G-4LYV12XS3G',
+  },};
