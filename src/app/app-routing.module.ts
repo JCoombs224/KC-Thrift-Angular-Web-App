a337ff5c38b8c0805069c8e7b77cb4a1db8cfdeb
Jamison C <jamiedude224@gmail.com>
2023-09-11 15:02:10 -0700

    - Updated mobile menu visuals - Added filters to shopping categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'shop/:category', component: ShopComponent },
+  { path: 'shop/:category/:subcategory', component: ShopComponent },
 ];
 
 @NgModule({
